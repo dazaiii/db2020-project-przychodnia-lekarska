26b1c761f4f79d4b8cfa7ef2e5000239b1721dbd
Kinga <58816347+dazaiii@users.noreply.github.com>
2020-05-30 19:44:38 +0200

    przychodniadb.sql
    Dodanie przykładowych danych do bazy

--- src/sql/przychodniadb.sql
+++ src/sql/przychodniadb.sql
@@ -94,11 +94,27 @@ CREATE TABLE Wizyta(
 /* Dodawanie */
 INSERT INTO Pacjent VALUES('1','Adam','Nowak','1920-02-11','20021112345','Krakow','123456789','an@mail.com',0);
 INSERT INTO Pacjent VALUES('2','Janusz','Kowalski','1960-05-11','60051112345','Krakow','123456789','jk@mail.com',1);
+INSERT INTO Pacjent VALUES('3','Maciej','Glowczyk','1981-04-12','81041212345','Tarnow','346283716','maciej_glowczyk@mail.com', 1);
+INSERT INTO Pacjent VALUES('4','Krzysztof','Rabczewski','1969-02-05','69020538261','Oswiecim','609752817','k_rabczewski@mail.com', 1);
+INSERT INTO Pacjent VALUES('5','Krzysztof','Nowakowski','1978-11-05','78110509675','Krakow','890765876','k_nowakowski@mail.com', 1);
+INSERT INTO Pacjent VALUES('6','Mikolaj','Mikolajowski','1990-12-11','90121178256','Krakow','6009876547','mikolajowski@mail.com', 1);
+INSERT INTO Pacjent VALUES('7','Mariusz','Budzyn','1929-01-01','21010176543','Niepolomice','789654321','budzyn@mail.com', 1);
+INSERT INTO Pacjent VALUES('8','Justyna','Kowal','1978-02-05','78020512765','Krakow','60098456728','justyna_kowal@mail.com', 1);
+INSERT INTO Pacjent VALUES('8','Zofia','Piotrowska','2001-10-10','01101076578','Niepolomice','789654678','piotrowksa@mail.com', 1);
+INSERT INTO Pacjent VALUES('9','Joanna','Maciszewska','1998-12-10','98121076238','Wieliczka','654789678','maciszewska@mail.com', 0);
+INSERT INTO Pacjent VALUES('10','Zygmunt','Wolak','1950-04-27','50042789765','Krakow','654789654','zygmunt_wolak@mail.com', 1);
 
-INSERT INTO Pracownik VALUES('1','Piotr','Madao','1940-02-11','20021112345','Krakow','123456789','madao@mail.com','Lekarz');
-INSERT INTO Pracownik VALUES('2','Krzysztof','Jarek','1959-02-11','59021112345','Krakow','123456789','kj@mail.com','Pielegniarka');
+INSERT INTO Pracownik VALUES('1','Piotr','Majewski','1940-02-11','40021167854','Krakow','123456789','majewski@mail.com','Lekarz');
+INSERT INTO Pracownik VALUES('2','Krzysztof','Jarczewski','1959-02-11','59021112345','Krakow','456787658','kj@mail.com','Lekarz');
+INSERT INTO Pracownik VALUES('3','Karolina','Malinowska','1967-04-04','67040454678','Oswiecim','567865456','malinowska@mail.com','Lekarz');
+INSERT INTO Pracownik VALUES('4','Malwina','Janiszewska','1987-05-20','87052087659','Krakow','567876908','janiszewska@mail.com','Lekarz');
+INSERT INTO Pracownik VALUES('5','Janina','Wojtaszewska','1969-03-15','69031512345','Wieliczka','123456789','wojtaszewska@mail.com','Pielegniarka');
+INSERT INTO Pracownik VALUES('5','Edyta','Wojcik','1970-05-15','70051512345','Tarnow','123456789','edyta_wojcik@mail.com','Pielegniarka');
 
-INSERT INTO Lekarz VALUES('1','Kardiolog','1','pełny',0);
+INSERT INTO Lekarz VALUES('1','Kardiolog','1','staz',0);
+INSERT INTO Lekarz VALUES('2','Dermatolog','10','pełny',0);
+INSERT INTO Lekarz VALUES('3','Pediatra','12','pełny',1);
+INSERT INTO Lekarz VALUES('4','Neurolog','5','pełny',0);
 
 INSERT INTO Gabinet VALUES('5','8','2','2020-05-20','20:50:00','1');
 
@@ -116,5 +132,6 @@ ALTER TABLE Uwagi ADD FOREIGN KEY (ID_wizyty) REFERENCES Wizyta(ID_wizyty);
 
 DROP USER IF EXISTS przychodniadb@localhost;
 
-CREATE USER przychodniadb@localhost IDENTIFIED BY 'Zaq12wsx';
+CREATE USER przychodniadb@localhost IDENTIFIED BY 'przychodniadb';
 GRANT ALL PRIVILEGES ON przychodniadb.* TO przychodniadb@localhost;
+FLUSH PRIVILEGES;
