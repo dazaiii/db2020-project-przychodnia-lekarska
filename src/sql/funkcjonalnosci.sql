b9da302b75e9caec9458c5306aa6f32cb95ea911
Karol <58816240+karol71927@users.noreply.github.com>
2020-06-12 21:33:10 +0200

    Funkcjonalności #1

--- src/sql/funkcjonalnosci.sql
+++ src/sql/funkcjonalnosci.sql
@@ -1,50 +1,93 @@
-/* 1.Dodaj receptę dla konkretnej wizyty */
+/* 1. Zarejestruj pacjenta */
 
-INSERT INTO recepta VALUES (21, "Apap", "Doustnie", 1);
+SELECT ubezpieczenie , ID_pacjenta FROM Pacjent WHERE Imie='%s' AND Nazwisko='%s'
 
-/* 2.Dodaj pacjenta do bazy */
-INSERT INTO pacjent VALUES (31, "Mateusz", "Nowak", 1993-03-10, "93031039487", "Kraków", 674356409, "nowak_mateusz@mail.com", 1);
+SELECT `lekarz`.`Urlop`, `gabinet`.`ID_pracownika`, `gabinet`.`ID_gabinetu` FROM `pracownik`
+INNER JOIN `lekarz` ON `lekarz`.`ID_pracownika` = `pracownik`.`ID_pracownika`
+INNER JOIN `gabinet` ON `gabinet`.`ID_pracownika` = `lekarz`.`ID_pracownika`
+WHERE `pracownik`.`Imie` = '%s' AND `pracownik`.`Nazwisko` = '%s';
 
-/* 3.Wypisz wszystkich lekarzy na urlopie */
+SELECT COUNT(`wizyta`.`ID_pracownika`) FROM `wizyta`
+WHERE `wizyta`.`ID_pracownika` = '%s' AND `wizyta`.`Godzina_wizyty` = '%s' AND
+`wizyta`.`Data_wizyty` = '%s';
+
+SELECT COUNT(ID_wizyty) FROM Wizyta;"
+
+INSERT INTO `wizyta` VALUES(%s, '%s', %s, %s, %s, '%s', '%s');
+
+/* 2. Wyswietl receptę */
+
+SELECT `pacjent`.`Imie`, `pacjent`.`Nazwisko`, `wizyta`.`ID_pacjenta`,
+`recepta`.`Nazwa_lekarstwa`, `recepta`.`Sposob_podania`
+FROM `pacjent`  LEFT JOIN `wizyta` ON `wizyta`.`ID_pacjenta` = `pacjent`.`ID_pacjenta`
+LEFT JOIN `recepta` ON `recepta`.`ID_wizyty` = `wizyta`.`ID_wizyty`
+WHERE `pacjent`.`Imie` = '%s' AND `pacjent`.`Nazwisko` = '%s';
+
+/* 3. Wypisz lekarzy na urlopie/nie na urlopie */
 
 SELECT pracownik.Imie, pracownik.Nazwisko, lekarz.Urlop
-FROM pracownik
-INNER JOIN lekarz ON lekarz.ID_pracownika = pracownik.ID_pracownika
-WHERE lekarz.Urlop = '1';
+FROM pracownik  INNER JOIN lekarz ON lekarz.ID_pracownika = pracownik.ID_pracownika
+WHERE lekarz.Urlop = '%s'
+ORDER BY Pracownik.Nazwisko ASC, Pracownik.Imie ASC;
 
-/* 4.Wypisz godziny przyjęć danego lekarza */
+/* 4. Wyświetl dane kontaktowe */
+
+SELECT Imie, Nazwisko, Telefon, Mail FROM %s;
+
+SELECT Imie, Nazwisko, Telefon, Mail FROM %s
+WHERE Imie = '%s' AND Nazwisko = '%s';
+
+/* 5. Wyświetl godziny przyjęć lekarzy */
 
 SELECT pracownik.Imie, pracownik.Nazwisko, lekarz.ID_pracownika, gabinet.Godzina_rozpoczecia, gabinet.Godzina_zakonczenia
-FROM pracownik
-INNER JOIN lekarz ON lekarz.ID_pracownika = pracownik.ID_pracownika
-INNER JOIN gabinet ON gabinet.ID_pracownika = lekarz.ID_pracownika;
+FROM pracownik  INNER JOIN lekarz ON lekarz.ID_pracownika = pracownik.ID_pracownika
+INNER JOIN gabinet ON gabinet.ID_pracownika = lekarz.ID_pracownika
+ORDER BY Pracownik.Nazwisko ASC, Pracownik.Imie ASC;
 
-/* 5.Wypisz dane kontaktowe danego lekarza */
+SELECT pracownik.Imie, pracownik.Nazwisko, lekarz.Specjalizacja, gabinet.Nr_gabinetu, gabinet.Pietro, gabinet.Godzina_rozpoczecia, gabinet.Godzina_zakonczenia
+FROM pracownik  INNER JOIN lekarz ON lekarz.ID_pracownika = pracownik.ID_pracownika
+INNER JOIN gabinet ON gabinet.ID_pracownika = lekarz.ID_pracownika
+WHERE pracownik.Imie = '%s' AND pracownik.Nazwisko = '%s';
 
-SELECT pracownik.Imie, pracownik.Nazwisko, lekarz.Specjalizacja, pracownik.Telefon, pracownik.Mail
-FROM pracownik
-INNER JOIN lekarz ON lekarz.ID_pracownika = pracownik.ID_pracownika
-WHERE pracownik.Imie = 'Krzysztof' AND pracownik.Nazwisko = 'Jarczewski';
+/* 6. Dodaj osobę do bazy */
 
-/* 6.Wypisz wizyty danego lekarza w danym dniu (To było dla piotra majewskiego, można mu dać więcej wizyt 08.06 aby było widać w programie) */
+SELECT COUNT(*) FROM %s;
+
+INSERT INTO `pacjent` (`ID_pacjenta`, `Imie`, `Nazwisko`, `Data_urodzenia`, `Pesel`, `Miejsce_zamieszkania`,
+`Telefon`, `Mail`, `Ubezpieczenie`) VALUES(%s, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');
+
+INSERT INTO `pracownik` (`ID_pracownika`, `Imie`, `Nazwisko`, `Data_urodzenia`, `Pesel`,
+Miejsce_zamieszkania`, `Telefon`, `Mail`, `Funkcja`)
+VALUES (%s, '%s', '%s', '%s', '%s', '%s', %s, '%s', '%s');
+
+/* 7. Wyświetl wizyty */
+
+SELECT pracownik.Imie, pracownik.Nazwisko, wizyta.Data_wizyty, wizyta.Godzina_wizyty, gabinet.Nr_gabinetu
+FROM pracownik  INNER JOIN wizyta ON wizyta.ID_pracownika = pracownik.ID_pracownika
+INNER JOIN gabinet ON wizyta.ID_gabinetu = gabinet.ID_gabinetu
+ORDER BY wizyta.data_wizyty ASC, wizyta.godzina_wizyty ASC;
 
 SELECT pracownik.Imie, pracownik.Nazwisko, wizyta.Data_wizyty, wizyta.Godzina_wizyty
-FROM pracownik
-INNER JOIN wizyta ON wizyta.ID_pracownika = pracownik.ID_pracownika
-WHERE pracownik.Imie = 'Piotr' AND pracownik.Nazwisko = 'Majewski' AND wizyta.Data_wizyty = '2020-06-08';
-
-/* 7.Wypisz wizyty w danym gabinecie */
+FROM pracownik  INNER JOIN wizyta ON wizyta.ID_pracownika = pracownik.ID_pracownika
+WHERE pracownik.Imie = '%s' AND pracownik.Nazwisko = '%s' AND wizyta.Data_wizyty = '%s'
+ORDER BY wizyta.Godzina_wizyty ASC;
 
 SELECT gabinet.Nr_gabinetu, gabinet.Pietro, wizyta.ID_wizyty, pacjent.Imie, pacjent.Nazwisko
 FROM gabinet
 LEFT JOIN wizyta ON wizyta.ID_gabinetu = gabinet.ID_gabinetu
-LEFT JOIN pacjent ON wizyta.ID_pacjenta = pacjent.ID_pacjenta
-WHERE gabinet.Nr_gabinetu = '31';
+LEFT JOIN pacjent ON wizyta.ID_pacjenta = pacjent.ID_pacjenta WHERE gabinet.Nr_gabinetu = '%s';
 
-/* 8.Wyświetl recepty danego pacjenta */
+/* 8. Wyświetl historię wizyt pacjenta */
 
-SELECT pacjent.Imie, pacjent.Nazwisko, wizyta.ID_pacjenta, recepta.Nazwa_lekarstwa, recepta.Sposob_podania
+SELECT pacjent.Imie, pacjent.Nazwisko, wizyta.Data_wizyty, wizyta.Godzina_wizyty FROM pacjent
+INNER JOIN wizyta ON wizyta.ID_pacjenta = pacjent.ID_pacjenta
+WHERE pacjent.Imie = '%s' AND pacjent.Nazwisko = '%s'
+ORDER BY Wizyta.Data_wizyty ASC, Wizyta.Godzina_wizyty ASC;
+
+/* 9. Wyświetl wszystkie skierowania danego pacjenta */
+
+SELECT pacjent.Imie, pacjent.Nazwisko, wizyta.ID_pacjenta, skierowanie.ID_skierowania, skierowanie.Typ
 FROM pacjent
-LEFT JOIN wizyta ON wizyta.ID_pacjenta = pacjent.ID_pacjenta
-LEFT JOIN recepta ON recepta.ID_wizyty = wizyta.ID_wizyty
-WHERE pacjent.Imie = 'Adam' AND pacjent.Nazwisko = 'Nowak';
+INNER JOIN wizyta ON wizyta.ID_pacjenta = pacjent.ID_pacjenta
+INNER JOIN skierowanie ON skierowanie.ID_wizyty = wizyta.ID_wizyty
+WHERE pacjent.Imie = '%s' AND pacjent.Nazwisko = '%s';
